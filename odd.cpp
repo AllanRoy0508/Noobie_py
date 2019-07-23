729e925e3dcd33b1c68a75d0671389ead8322996
AllanRoy0508 <52188951+AllanRoy0508@users.noreply.github.com>
2019-07-23 14:35:23 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  if(n==0)
+  {
+    cout<<Zero;
+  }
+  elseif((n%2)==0)
+  {
+    cout<<Even;
+  }
+  else
+  {
+    cout<<Odd;
+  }
+  return 0;
+}
+  
